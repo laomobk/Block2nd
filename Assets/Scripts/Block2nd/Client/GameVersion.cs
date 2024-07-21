8314288b1ee86d6e47675f67a5a5f0ba22ce4d3c
LaomoBK <laomobk@163.com>
2024-07-22 02:12:07 +0800

    version(0.3.15)

--- Assets/Scripts/Block2nd/Client/GameVersion.cs
+++ Assets/Scripts/Block2nd/Client/GameVersion.cs
@@ -2,7 +2,7 @@
 {
     public static class GameVersion
     {
-        public static readonly string Version = "0.3.15dev";
-        public static readonly string Subtitle = "Infdev - dev_lightsystem";
+        public static readonly string Version = "0.3.15";
+        public static readonly string Subtitle = "Infdev";
     }
 }
